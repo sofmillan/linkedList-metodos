069e0ceb649db7e903ff816d302c9ff03d995cb9
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-02-28 15:42:31 -0500

    First commit

---
+++ src/LinkedList.java
@@ -0,0 +1,131 @@
+public class LinkedList {
+
+    class Node {
+        int value;
+        Node next = null;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        Node() {
+        }
+    }
+
+    protected Node head = null;
+    protected Node tail = null;
+
+    public void agregarInicio(int valor) {
+        Node nuevoNodo = new Node(valor);
+        nuevoNodo.next = head;
+        head = nuevoNodo;
+        if (nuevoNodo.next == null) {
+            tail = nuevoNodo;
+        }
+    }
+
+    public void agregarFinal(int valor) {
+        Node nuevoNodo = new Node(valor);
+        if (tail == null) {
+            head = nuevoNodo;
+        } else {
+            tail.next = nuevoNodo;
+            tail = nuevoNodo;
+        }
+    }
+
+
+
+    public void agregarPorPosicion(int posicion, int valor) {
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (posicion == 0) {
+            agregarInicio(valor);
+        } else {
+            Node nuevoNodo = new Node(valor);
+            Node actual = head;
+            for (int i = 0; i < posicion - 1; i++) {
+                if (actual == null) {
+                    throw new IndexOutOfBoundsException();
+                }
+                actual = actual.next;
+            }
+            if (actual.next == null) {
+                agregarFinal(valor);
+            } else {
+                nuevoNodo.next = actual.next;
+                actual.next = nuevoNodo;
+            }
+        }
+
+    }
+
+    public boolean contieneValor(int valor) {
+        Node nuevoNodo = new Node(valor);
+        Node actual = head;
+        while(actual!=null){
+            if(actual.value==nuevoNodo.value){
+                return true;
+            }
+            actual=actual.next;
+        }
+        return false;
+    }
+
+    public int obtenerPorPosicion(int posicion){
+        Node actual = head;
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (posicion == 0) {
+            return actual.value;
+        } else {
+            for (int i = 0; i < posicion ; i++){
+                if(actual==null){
+                    throw new IndexOutOfBoundsException();
+                }
+                actual=actual.next;
+            }
+            return actual.value;
+        }
+
+    }
+
+
+    public void eliminarInicio(){
+        head = head.next;
+    }
+
+    public void eliminarFinal(){
+        Node actual = head;
+        while(actual.next.next!=null){
+            actual=actual.next;
+        }
+        actual.next=null;
+    }
+
+    public void eliminarPorPosicion(int posicion){
+        Node actual = head;
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (posicion == 0) {
+           eliminarInicio();
+        } else {
+            for (int i = 0; i < posicion-1 ; i++){
+                if(actual==null){
+                    throw new IndexOutOfBoundsException();
+                }
+                actual=actual.next;
+            }
+            actual.next=actual.next.next;
+        }
+    }
+
+    public void recorrer(){
+        Node actual = head;
+        while(actual!=null){
+            System.out.println(actual.value);
+            actual=actual.next;
+        }
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,54 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.agregarInicio(61);
+        linkedList.agregarFinal(30);
+        linkedList.agregarPorPosicion(1, 14);
+        linkedList.agregarPorPosicion(2, 18);
+
+        //Método contiene
+       System.out.println(linkedList.contieneValor(30));
+
+
+
+        //Método obtener por posición
+       System.out.println(linkedList.obtenerPorPosicion(3));
+
+
+
+       //Método eliminar al inicio
+        System.out.println("Antes de eliminar");
+        linkedList.recorrer();
+
+        linkedList.eliminarInicio();
+
+        System.out.println("Después de eliminar");
+        linkedList.recorrer();
+
+
+
+        //Método eliminar al final
+        System.out.println("Antes de eliminar");
+        linkedList.recorrer();
+
+        linkedList.eliminarFinal();
+
+        System.out.println("Después de eliminar");
+        linkedList.recorrer();
+
+
+
+        //Método eliminar por posición
+          System.out.println("Antes de eliminar");
+        linkedList.recorrer();
+
+        linkedList.eliminarPorPosicion(3);
+
+        System.out.println("Después de eliminar");
+        linkedList.recorrer();
+
+    }
+}
